ec4e4ff9df5b12907f6ab79bb5c207796005947f
Jocelyn <59553647+JocelynMilRodriguez@users.noreply.github.com>
2020-04-10 12:44:42 -0400

    Update ViewController.swift

--- GradingSystem/GradingSystem/ViewController.swift
+++ GradingSystem/GradingSystem/ViewController.swift
@@ -1,6 +1,7 @@
-//
+
 //  ViewController.swift
-//  Project: GradingSystem
+//  GradingSystem
+//
 //  Created by Jocelyn M. Rodriguez on 4/8/20.
 //  Copyright © 2020 CUNY Borough of Manhattan Community College. All rights reserved.
 //
@@ -8,29 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
         
-        override func viewDidLoad() {
-               super.viewDidLoad()
-               // Do any additional setup after loading the view.
-               
-               let someGrade: Int = 86;
-               
-               if someGrade > 100 {
-                   print("Impossible!");
-               } else if someGrade >= 89 {
-                   print("Your grade is an A");
-               } else if someGrade >= 79 {
-                   print("Your grade is an B");
-               } else if someGrade >= 69 {
-                   print("Your grade is an C");
-               } else if someGrade >= 65 {
-                   print("Your grade is an D");
-               } else {
-                   print("Your grade is an F. You must retake this class.")
-               }
+        super.viewDidLoad()
+           // Do any additional setup after loading the view.
+           
+           let someGrade: Int = 86;
+           
+           if someGrade > 100 {
+               print("Impossible!");
+           } else if someGrade >= 89 {
+               print("Your grade is an A");
+           } else if someGrade >= 79 {
+               print("Your grade is an B");
+           } else if someGrade >= 69 {
+               print("Your grade is an C");
+           } else if someGrade >= 65 {
+               print("Your grade is an D");
+           } else {
+               print("Your grade is an F. You must retake this class.")
            }
+        
     }
-
-
-}
-
